02e2b5a3e6edade422b1078e5e29c04c71f98df6
chillale <chillalesravankumar1995@gmail.com>
2022-10-19 17:47:19 +0530

    pushing the code to github

---
+++ Sdet123/src/test/java/venkat/Venkat.java
@@ -0,0 +1,8 @@
+package venkat;
+
+public class Venkat {
+	public static void main(String[] args) {
+		System.out.println("venaktt");
+	}
+
+}
---
+++ Sdet123/src/test/java/venkat/Venkat1.java
@@ -0,0 +1,8 @@
+package venkat;
+
+public class Venkat1 {
+	public static void main(String[] args) {
+		System.out.println("kalika");
+	}
+
+}
